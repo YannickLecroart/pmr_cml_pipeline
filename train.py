5586e2876d3434ac6845e4171eb0d2b49950e696
Yannick Lecroart <43994829+YannickLecroart@users.noreply.github.com>
2021-06-17 09:19:02 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,22 @@
 import joblib
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 def train_model():
 
     x_train_data = np.load("./x_train.npy", allow_pickle=True)
     y_train_data = np.load("./y_train.npy", allow_pickle=True)
+    x_test_data = np.load("./x_test.npy", allow_pickle=True)
+    y_test_data = np.load("./y_test.npy", allow_pickle=True)
 
     model = RandomForestClassifier(n_estimators = 1000, max_depth=10, verbose=1) #depth 4
     model.fit(x_train_data, y_train_data)
+    
+    model = XGBClassifier(objective='reg:logistic', n_estimators=1000, learning_rate=0.1)
+    model.fit(x_train_data, y_train_data,
+                    eval_set=[(x_train_data, y_train_data), (x_test_data, y_test_data)],
+                    early_stopping_rounds=100,
+                verbose=True)
     
     joblib.dump(model, 'model.pkl')
 
